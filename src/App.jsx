e99865a5dcc844bfaf33e339ef428a63338a956e
Sonya Castro <sonya.castro.gomez@gmail.com>
2023-10-16 22:30:12 -0500

    feat: header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import "./App.scss";
 
+import { AboutCard } from './components/Card/Card';
+
 function App() {
   return (<>
+    <header>
 
+      <img className="profile" src="/assets/images/Profile.svg" alt="profile" />
+      <AboutCard />
+      <div className="figure rectangle" />
+      <div className="figure circle" />
+      <img id="grid1" className="figure dot-grid" src="/assets/images/dot-grid.svg" alt="" />
+      <img id="grid2" className="figure dot-grid" src="/assets/images/dot-grid.svg" alt="" />
+
+    </header>
   </>);
 }
 
--- src/components/Profile/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import './Profile.scss';
-
-const Profile = () => <div className='profile'>
-    <div className='rectangle' />
-    <div className='elipse' />
-    <img src="/assets/images/profile.png" alt="profile" />
-</div>;
-
-export default Profile;
